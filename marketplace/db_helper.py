0795da824aad0d03a5cda056f3883f3a60926450
aatharvauti <bm.aatharvauti@protonmail.ch>
2022-04-14 12:42:07 +0530

    Add marketplace/db_helper.py

---
+++ marketplace/db_helper.py
@@ -0,0 +1,44 @@
+# https://console.clever-cloud.com/
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+# VENV Importing
+host = os.getenv('HOST')
+db = os.getenv('DB')
+user = os.getenv('USER')
+port = os.getenv('PORT')
+password = os.getenv('PASSWORD')
+uri = os.getenv('URI')
+
+# pip install mysql-connector-python
+import mysql.connector
+
+class DBHelper:
+
+    def __init__(self):
+        # Create Connection
+        self.mydb = mysql.connector.connect(
+            host=host,
+            port=port,
+            user=user,
+            password=password,
+            database=db
+        )
+    
+    def __del__(self):
+        # Close Connection
+        self.mydb.close()
+
+    def execute_select_query(self, query):
+        # Create cursor 
+        mycursor = self.mydb.cursor(dictionary=True)
+
+        # Fire Query
+        mycursor.execute(query)
+
+        # Extract result
+        myresult = mycursor.fetchall()
+
+        return myresult
